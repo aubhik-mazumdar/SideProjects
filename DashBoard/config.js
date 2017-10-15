20804a33e0cbc4edec9c0ef7085525bad1032987
Vic <mail@testsite-vic.ml>
2017-10-15 17:21:00 +0200

    Create config.js

---
+++ DashBoard/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	weather: {
+		apiKey: '***'
+	},
+
+	todo: {
+		password: '***'
+	},
+
+	news: {
+		apiKey: '***'
+	},
+
+	github: {
+		username: 'YOURUSERNAME',
+		password: '***'
+	},
+
+	webInterface: {
+		username: 'YOURNAME'
+	}
+}
