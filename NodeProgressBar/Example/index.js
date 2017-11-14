61e5a7fb9115acfd274e6b55f2ed5727bfe6c3b9
jantje19 <mail@testsite-vic.ml>
2017-11-14 12:35:07 +0100

    Update

--- NodeProgressBar/Example/index.js
+++ NodeProgressBar/Example/index.js
@@ -1,38 +1,4 @@
-// const Progress = require('./Progress.js');
-
-// const Express = require('express');
-// const app = Express();
-
-// app.get('*progress*', (request, response) => {
-// 	let i = 0;
-// 	const p = new Progress(response);
-
-// 	const int = setInterval(() => {
-// 		console.log(i);
-// 		if (i > 10) {
-// 			p.close();
-// 			clearInterval(int);
-// 		} else {
-// 			p.update(i / 10 * 100);
-// 			i++;
-// 		}
-// 	}, 1000);
-// });
-
-// app.get('*', (request, response) => {
-// 	response.sendFile(__dirname + '/main.html');
-// });
-
-// app.listen(8000, err => {
-// 	if (err)
-// 		console.error(err);
-// 	else
-// 		console.log('Running on port 8000');
-// });
-
-
-
-const Progress = require('express-progressbar');
+const Progress = require('../Progress.js');
 const Express = require('express');
 const app = Express();
 
